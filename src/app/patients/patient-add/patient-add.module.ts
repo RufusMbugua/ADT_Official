c035bca269402d7907d0ae2a1ff1395a2df644c0
Brian Mwathi <mwathibrian7@gmail.com>
2016-11-02 15:14:14 +0300

    Setup the Add Patients Module (Date picker not working)

--- src/app/patients/patient-add/patient-add.module.ts
+++ src/app/patients/patient-add/patient-add.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SmartadminModule} from "../../shared/smartadmin.module";
 import {PatientAddComponent} from "./patient-add.component";
+import { formPluginsRouting } from '../../+forms/+form-plugins/form-plugins.routing';
+import {XEditableWidgetComponent} from "../../+forms/+form-plugins/x-editable-widget/x-editable-widget.component";
+import {DuallistboxWidgetComponent} from "../../+forms/+form-plugins/duallistbox-widget/duallistbox-widget.component";
+import {SmartadminModule} from "../../shared/smartadmin.module";
+import {SmartadminInputModule} from "../../shared/forms/input/smartadmin-input.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    
-      SmartadminModule
+    formPluginsRouting,
+    SmartadminModule,
+    SmartadminInputModule
   ],
-  declarations: [PatientAddComponent]
+  declarations: [PatientAddComponent, XEditableWidgetComponent, DuallistboxWidgetComponent]
 })
 export class PatientAddModule { }
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -6,6 +6,7 @@ declare var $: any;
 @Component({
   selector: 'sa-header',
   templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
