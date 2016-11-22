ce6cbddbe3bdbdc5eb00557e8b43b8839e1092a1
[anamikoye] <[anamikoye52@gmail.com]>
2016-11-22 17:31:39 +0300

    Dispense Beginnings

--- src/app/patients/patient-dispense/patient-dispense.module.ts
+++ src/app/patients/patient-dispense/patient-dispense.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { patientdispenseRouting } from './patient-dispense.routing';
 import { PatientDispenseComponent } from './patient-dispense.component';
 import { SmartadminModule } from "../../shared/smartadmin.module";
+import {SmartadminInputModule} from "../../shared/forms/input/smartadmin-input.module";
 
 @NgModule({
     imports: [
         CommonModule,
         patientdispenseRouting,
-        SmartadminModule
+        SmartadminModule,
+        SmartadminInputModule
     ],
     exports: [],
     declarations: [PatientDispenseComponent],
